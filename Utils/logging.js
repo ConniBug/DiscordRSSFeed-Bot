6ddf9c946bae475b955157c3bb9c2a3e38dab46a
Conni <connithekiwi@gmail.com>
2021-04-12 03:49:34 +0100

    Email support.

---
+++ Utils/logging.js
@@ -0,0 +1,105 @@
+require("colors");
+const sendMail = require("./mailer").sendMail;
+
+logLevel = "LEGITALL";
+function getLogLevelNum(level) {
+  if (level == "TESTING") return 0;
+  if (level == "GENERIC") return 1;
+  if (level == "ERROR") return 2;
+  if (level == "DEBUG") return 3;
+  if (level == "ALL") return 4;
+
+  // Debugging stuff.
+  if (level == "TIMINGS") return 5;
+
+  if (level == "LEGITALL") return 100;
+
+  log("Unsure what log level " + level.red + " belongs to.", "GENERIC");
+  return 4;
+}
+exports.getLogLevelNum = (level) => {
+  return getLogLevelNum(level);
+};
+async function log(message, type = "DEBUG") {
+  if (getLogLevelNum(type) > getLogLevelNum(logLevel)) {
+    return;
+  }
+
+  maxSize = 45;
+
+  time = getDateTime().yellow;
+
+  StartMessage = "";
+  if (type == "ERROR") {
+    StartMessage = `[${time}] - [` + type.red + `]`;
+    var build = `
+    Time: ${getDateTime()}
+    <br>
+    <br>
+    <div>
+    ${message}
+    </div>
+    `;
+    sendMail(process.env.ADMIN_EMAIL, build,"RSSFeed-Bot - Server Error");
+    if(process.env.ADMIN_EMAIL_2) {
+        sendMail(process.env.ADMIN_EMAIL_2, build,"RSSFeed-Bot - Server Error");
+    }
+  } else if (type == "GENERIC")
+    StartMessage = `[${time}] - [` + type.green + `]`;
+  else if (type == "DEBUG") StartMessage = `[${time}] - [` + type.gray + `]`;
+  else if (type == "TESTING")
+    StartMessage = `[${time}] - [` + type.magenta + `]`;
+  else StartMessage = `[${time}] - [` + type.blue + `]`;
+
+  left = maxSize - StartMessage.length;
+  function balence() {
+    tmp = "";
+    space = " ";
+    while (left >= 0) {
+      left = left - 1;
+      tmp = tmp + space;
+    }
+    return tmp;
+  }
+  console.log(StartMessage + balence() + "-> " + message);
+}
+exports.log = async (message, type = "DEBUG") => {
+  log(message, type);
+};
+function char_count(str, letter) {
+  var letter_Count = 0;
+  for (var position = 0; position < str.length; position++) {
+    if (str.charAt(position) == letter) {
+      letter_Count += 1;
+    }
+  }
+  return letter_Count;
+}
+exports.char_count = (str, letter) => {
+  return char_count(str, letter);
+};
+function getDateTime() {
+  var date = new Date();
+
+  var hour = date.getHours();
+  hour = (hour < 10 ? "0" : "") + hour;
+
+  var min = date.getMinutes();
+  min = (min < 10 ? "0" : "") + min;
+
+  var sec = date.getSeconds();
+  sec = (sec < 10 ? "0" : "") + sec;
+
+  var year = date.getFullYear();
+
+  var month = date.getMonth() + 1;
+  month = (month < 10 ? "0" : "") + month;
+
+  var day = date.getDate();
+  day = (day < 10 ? "0" : "") + day;
+
+  return year + ":" + month + ":" + day + " - " + hour + ":" + min + ":" + sec;
+}
+exports.getDateTime = () => {
+  return getDateTime();
+};
--- index.js
+++ index.js
@@ -4,294 +4,357 @@ const client = new Discord.Client();
 const Parser = require('rss-parser');
 const cron = require("node-cron");
 const path = require('path');
+const sendMail = require("./Utils/mailer").sendMail;
 
-// Config
-var botStatus = "Rawr!~";
-var prefix = "-";
-var guildID = "";
-var channelID = "";
+require("dotenv").config();
+
+const logging = require("./Utils/logging");
+
+BOT = false;
 
 // Vars
-var times  = [];
+var GUIDs  = [];
 var obects = [];
 
-
-client.on("ready", async () => {   
-    console.log(`Bot has started, with ${client.users.cache.size} users, in ${client.channels.cache.size} channels of ${client.guilds.cache.size} guilds.`);  
-    client.user.setActivity(botStatus);
+fs.readFile('sent.json', (err, data) => {
+    if (err) throw err;
+    let dat = JSON.parse(data);
+    dat.guids.forEach(e => {
+      GUIDs.push(e);
+      console.log("Already sent guid:",e);
+    });
 });
 
-cron.schedule('* * * * *', () => {
+console.log('This is after the read call');
+
+cron.schedule('* * * * * *', () => {
   let parser = new Parser();
 
-  const directoryPath = path.join(__dirname, './feeds/');
-  fs.readdir(directoryPath, function (err, files) {
-      if (err) return console.log('Unable to scan directory: ' + err);
-      files.forEach(function (file) {
+  try {
+    const directoryPath = path.join(__dirname, './feeds/');
+    fs.readdir(directoryPath, function (err, files) {
+        if (err) return console.log('Unable to scan directory: ' + err);
+        files.forEach(function (file) {
+            let rawdata = fs.readFileSync(`./feeds/${file}`);
+            let obj = JSON.parse(rawdata);
 
-          let rawdata = fs.readFileSync(`./feeds/${file}`);
-          let obj = JSON.parse(rawdata);
+            var FEED_LIST = [
+              obj.rssURL
+            ];
+            
+            var titleKeyName = obj.titleKey;
+            var date  = obj.publishDateKey;
 
-          var FEED_LIST = [
-            obj[0]
-          ];
-          
-          var title = obj[1];
-          var date  = obj[2];
+            const feedRequests = FEED_LIST.map(feed => {
+              return parser.parseURL(feed);
+            });
+      
+            Promise.all(feedRequests).then(response => {
+              var KeyInfo = response[0].items[0];
+              //console.log("KeyInfo", KeyInfo);
+              // if(times.includes(KeyInfo[date])) {
+              //   return;
+              // }
 
-          obj = obj.splice(3);
-    
+              // var Embed = new Discord.MessageEmbed()
+              //     .setTitle(`RSS - ${KeyInfo[titleKeyName]}`)
+              //     .setURL(obj[0])
+              //     .setColor("#00FF00"); 
 
-          const feedRequests = FEED_LIST.map(feed => {
-            return parser.parseURL(feed);
-          });
-    
-          Promise.all(feedRequests).then(response => {
-            var KeyInfo = response[0].items[0];
-    
-            var Embed = new Discord.MessageEmbed()
-                .setTitle(`RSS - ${title}`)
-                .setColor("#00FF00"); 
-   
-            if(obects.includes(obj[0]))
-            {
-              if(KeyInfo[date] === times[obects.indexOf(obj[0])]) return;
-            }
-            else
-            {
-              obects.push(obj[0]);
-              times.push(KeyInfo[date]);
-            }
+              var exists = false;
+              GUIDs.forEach(e => {
+                if(e == KeyInfo[obj.guid]) {
+                  exists = true;
+                };
+              });
+              if(exists) {
+                //console.log("Already sent.");
+                return;
+              }
+              
+              GUIDs.push(KeyInfo[obj.guid]);
 
-            obj.forEach(KeyTitle =>
+              fs.writeFileSync('sent.json', JSON.stringify({ "guids": GUIDs }));
+
+              // if(obects.includes(obj.guid))
+              // {
+              //   if(KeyInfo[date] === GUIDs[obects.indexOf(obj.guid)]) 
+              //     return;
+              // }
+              // else
+              // {
+              //   obects.push(obj.rssURL);
+              //   times.push(KeyInfo[date]);
+              // }
+
+              var build = `Date: ${KeyInfo[date]}<br><br>`;
+
+              console.log("Title:", `RSS - ${KeyInfo[titleKeyName]}`);
+
+              // console.log("-------------------");
+              // console.log("obj.otherKeys");
+              // console.log(obj.otherKeys);
+              // console.log("-------------------");
+              // console.log("KeyInfo");
+              // console.log(KeyInfo);
+
+              obj.otherKeys.forEach(KeyTitle =>
               {
-                Embed.addField(KeyTitle, "" + KeyInfo[KeyTitle] + "");
+                console.log(KeyTitle, " - " + KeyInfo[KeyTitle] + "");
+
+                build += `${KeyTitle}<br>${KeyInfo[KeyTitle]}<br><br>`;
+                // Embed.addField(KeyTitle, " - " + KeyInfo[KeyTitle] + "");
               });
 
-            let channel1 = client.guilds.cache.find(c => c.id == guildID).channels.cache.find(c => c.id == channelID)
-                .send(Embed);        
-          });
-      });
-  });
+              console.log("SENDING:", build);
+              sendMail(process.env.ADMIN_EMAIL  , build,`RSS - ${KeyInfo[titleKeyName]}`);
+              sendMail(process.env.ADMIN_EMAIL_2, build,`RSS - ${KeyInfo[titleKeyName]}`);
+
+
+
+              // let channel1 = client.guilds.cache.find(c => c.id == guildID).channels.cache.find(c => c.id == channelID)
+              //   .send(Embed);     
+
+            });
+        });
+    });
+ 
+  } catch (error) {
+    // console.error("ERROR", error);
+    logging.log(error, "ERROR");
+  }
 });
 
-client.on("message", async message => {
-    if (message.author.bot) return;  
+if(BOT) {
+  // Config
+  var botStatus = "Rawr!~";
+  var prefix = "-";
+  var guildID = "";
+  var channelID = "";
 
-    const args = message.content.slice(prefix.length).trim().split(/ +/g);
-    const command = args.shift().toLowerCase();
-    
-    if(command === `setup`)
-    {
-      let parser = new Parser();
-
-      var FEED_LIST = [
-        args[0]
-      ];
-      
-      console.log(args[0]);
-      const feedRequests = FEED_LIST.map(feed => {
-        return parser.parseURL(feed);
-      })
-      
-      Promise.all(feedRequests).then(response => {
-        var recent = response[0].items[0];
-        // Debugging // console.log(recent);
+  client.on("ready", async () => {   
+      console.log(`Bot has started, with ${client.users.cache.size} users, in ${client.channels.cache.size} channels of ${client.guilds.cache.size} guilds.`);  
+      client.user.setActivity(botStatus);
+  });
+  
+  client.on("message", async message => {
+      if (message.author.bot) return;  
+  
+      const args = message.content.slice(prefix.length).trim().split(/ +/g);
+      const command = args.shift().toLowerCase();
+     
+      if(command === `setup`)
+      {
+        let parser = new Parser();
+  
+        var FEED_LIST = [
+          args[0]
+        ];
+       
+        console.log(args[0]);
+        const feedRequests = FEED_LIST.map(feed => {
+          return parser.parseURL(feed);
+        })
+       
+        Promise.all(feedRequests).then(response => {
+          var recent = response[0].items[0];
+          // Debugging // console.log(recent);
+          var Embed = new Discord.MessageEmbed();
+          Embed.setTitle("Keys");
+          Embed.setColor("#00FF00");
+  
+          var i = 0;
+          Object.keys(recent).forEach(t => {
+            // Debugging // console.log(t);
+  
+            // 2 Varients of displaying the keys data
+            // banForm.setDescription(banForm.description + ": " + recent[t] + "\n" + t)
+            Embed.addField(t, "```" + recent[t] + "```");
+            i++;
+          });
+          message.reply(Embed);
+  
+  
+        })
+      }
+  
+      if(command === `save`)
+      {
+        let p2 = args.slice(3);
+        console.log(args);
+        console.log(p2);
+        
         var Embed = new Discord.MessageEmbed();
-        Embed.setTitle("Keys");
-        Embed.setColor("#00FF00");
-
-        var i = 0;
-        Object.keys(recent).forEach(t => {
-          // Debugging // console.log(t);
-
-          // 2 Varients of displaying the keys data
-          // banForm.setDescription(banForm.description + ": " + recent[t] + "\n" + t)
-          Embed.addField(t, "```" + recent[t] + "```");
-          i++;
-        });
-        message.reply(Embed);
-
-
-      })
-    }
-
-    if(command === `save`)
-    {
-      let p2 = args.slice(3);
-      console.log(args);
-      console.log(p2);
+        Embed.setTitle(`Saved - ${args[1]}` || "Saved - RSS Feed");
+        Embed.setDescription(args[0])
+        Embed.setColor("#00FF00");  
+  
+        var j = "";
+        p2.forEach(r => j = j + r + ",");
+        j = j.substring(0, j.length - 1);
+  
+        Embed.addField("Keys", j);
+  
+        message.channel.send(Embed);
+  
+        require('fs').writeFile(
+  
+          `./feeds/${args[1]}.json`,
       
-      var Embed = new Discord.MessageEmbed();
-      Embed.setTitle(`Saved - ${args[1]}` || "Saved - RSS Feed");
-      Embed.setDescription(args[0])
-      Embed.setColor("#00FF00");  
-
-      var j = "";
-      p2.forEach(r => j = j + r + ",");
-      j = j.substring(0, j.length - 1);
-
-      Embed.addField("Keys", j);
-
-      message.channel.send(Embed);
-
-      require('fs').writeFile(
-
-        `./feeds/${args[1]}.json`,
-    
-        JSON.stringify(args),
-    
-        function (err) {
-            if (err) {
-                console.error(err);
-            }
-        }
-      );
-
-    }
-
-    if(command === `onload`)
-    {
-      let parser = new Parser();
-
-      const path = require('path');
-      const directoryPath = path.join(__dirname, './feeds/');
-      fs.readdir(directoryPath, function (err, files) {
-          if (err) return console.log('Unable to scan directory: ' + err);
-
-          files.forEach(function (file) {
-              // Do whatever you want to do with the file
-              console.log(file); 
-
-
-              let rawdata = fs.readFileSync(`./feeds/${file}`);
-              let obj = JSON.parse(rawdata);
-
-              var FEED_LIST = [
-                obj[0]
-              ];
-              
-              var title = obj[1];
-              var date  = obj[2];
-              
-              if(obects.includes(obj[0]))
-              {
-                if(times[obects.indexOf(obj[0])] != date)
+          JSON.stringify(args),
+      
+          function (err) {
+              if (err) {
+                  console.error(err);
+              }
+          }
+        );
+  
+      }
+  
+      if(command === `onload`)
+      {
+        let parser = new Parser();
+  
+        const path = require('path');
+        const directoryPath = path.join(__dirname, './feeds/');
+        fs.readdir(directoryPath, function (err, files) {
+            if (err) return console.log('Unable to scan directory: ' + err);
+  
+            files.forEach(function (file) {
+                // Do whatever you want to do with the file
+                console.log(file); 
+  
+  
+                let rawdata = fs.readFileSync(`./feeds/${file}`);
+                let obj = JSON.parse(rawdata);
+  
+                var FEED_LIST = [
+                  obj[0]
+                ];
+                
+                var title = obj[1];
+                var date  = obj[2];
+                
+                if(obects.includes(obj[0]))
                 {
-                  console.log("-Not Same----------");
-                  console.log(times[obects.indexOf(obj[0])]);
-                  console.log(date);
-                  console.log("-----------");
+                  if(times[obects.indexOf(obj[0])] != date)
+                  {
+                    console.log("-Not Same----------");
+                    console.log(times[obects.indexOf(obj[0])]);
+                    console.log(date);
+                    console.log("-----------");
+                  }
+                  else
+                  {
+                    console.log("-Same----------");
+                    console.log(times[obects.indexOf(obj[0])]);
+                    console.log(date);
+                    console.log("-----------");
+                    return;
+                  }
+                  
                 }
                 else
                 {
-                  console.log("-Same----------");
-                  console.log(times[obects.indexOf(obj[0])]);
-                  console.log(date);
-                  console.log("-----------");
-                  return;
+                  obects.push(obj[0]);
+                  times.push(date);
+  
                 }
-                
-              }
-              else
-              {
-                obects.push(obj[0]);
-                times.push(date);
-
-              }
-
-              obj = obj.splice(3);
-        
-
-              const feedRequests = FEED_LIST.map(feed => {
-                return parser.parseURL(feed);
-              });
-        
-              Promise.all(feedRequests).then(response => {
+  
+                obj = obj.splice(3);
+          
+  
+                const feedRequests = FEED_LIST.map(feed => {
+                  return parser.parseURL(feed);
+                });
+          
+                Promise.all(feedRequests).then(response => {
+                  var KeyInfo = response[0].items[0];
+                  // Debugging // console.log(recent);
+          
+                  var Embed = new Discord.MessageEmbed();
+                  Embed.setTitle(`RSS - ${title}`);
+                  Embed.setColor("#00FF00");  
+          
+                  obj.forEach(KeyTitle =>
+                    {
+                      Embed.addField(KeyTitle, "" + "-" + KeyInfo[KeyTitle] + "-" + "");
+                      // Debugging // console.log(KeyTitle);
+          
+                    
+                    });
+                  
+                  Embed.setTimestamp();
+                  message.reply(Embed);
+          
+                });
+            });
+        });
+      }
+  
+      if(command === `remove`)
+      {
+        const path = `./feeds/${args[0]}.json`
+  
+        fs.unlink(path, (err) => {
+          if (err) {
+            console.error(err)
+            return
+          }              
+          var Embed = new Discord.MessageEmbed()
+              .setTitle(`Removed - ${args[0]}`);
+          message.reply(Embed);
+  
+          //file removed
+        })
+      }
+  
+      if(command === `ttt`)
+      {
+        let parser = new Parser();
+  
+        obects.forEach(obj =>
+          {
+  
+            console.log(obj);
+  
+            var FEED_LIST = [
+              obj[0]
+            ];
+  
+            obj = obj.splice(3);
+  
+  
+            const feedRequests = FEED_LIST.map(feed => {
+              return parser.parseURL(feed);
+            });
+  
+            Promise.all(feedRequests).then(response => {
                 var KeyInfo = response[0].items[0];
-                // Debugging // console.log(recent);
-        
-                var Embed = new Discord.MessageEmbed();
-                Embed.setTitle(`RSS - ${title}`);
-                Embed.setColor("#00FF00");  
-        
+  
+                var Embed = new Discord.MessageEmbed()
+                  .setTitle(`RSS - ${obj[1]}`)
+                  .setColor("#00FF00")
+                  .setTimestamp();
+  
                 obj.forEach(KeyTitle =>
                   {
                     Embed.addField(KeyTitle, "" + "-" + KeyInfo[KeyTitle] + "-" + "");
-                    // Debugging // console.log(KeyTitle);
-        
-                  
+                    console.log(KeyTitle);
                   });
                 
-                Embed.setTimestamp();
                 message.reply(Embed);
-        
-              });
-          });
-      });
-    }
-
-    if(command === `remove`)
-    {
-      const path = `./feeds/${args[0]}.json`
-
-      fs.unlink(path, (err) => {
-        if (err) {
-          console.error(err)
-          return
-        }              
-        var Embed = new Discord.MessageEmbed()
-            .setTitle(`Removed - ${args[0]}`);
-        message.reply(Embed);
-
-        //file removed
-      })
-    }
-
-    if(command === `ttt`)
-    {
-      let parser = new Parser();
-
-      obects.forEach(obj =>
-        {
-
-          console.log(obj);
-
-          var FEED_LIST = [
-            obj[0]
-          ];
-
-          obj = obj.splice(3);
-
-
-          const feedRequests = FEED_LIST.map(feed => {
-            return parser.parseURL(feed);
-          });
-
-          Promise.all(feedRequests).then(response => {
-              var KeyInfo = response[0].items[0];
-
-              var Embed = new Discord.MessageEmbed()
-                .setTitle(`RSS - ${obj[1]}`)
-                .setColor("#00FF00")
-                .setTimestamp();
-
-              obj.forEach(KeyTitle =>
-                {
-                  Embed.addField(KeyTitle, "" + "-" + KeyInfo[KeyTitle] + "-" + "");
-                  console.log(KeyTitle);
-                });
-              
-              message.reply(Embed);
-            }
-          )
-      })
-    }
-
-});
-
-client.on("error", (e) => console.error(e));
-client.on("warn", (e) => console.warn(e));
-client.on("debug", (e) => console.info(e));
-
-client.login("Bot id here!!!!");
+              }
+            )
+        })
+      }
+  
+  });
+  
+  client.on("error", (e) => console.error(e));
+  client.on("warn", (e) => console.warn(e));
+  client.on("debug", (e) => console.info(e));
+  
+  client.login("Bot id here!!!!");
+}
